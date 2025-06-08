7eec33e9b8315d26ac029869ff973c6a61334511
Anjelita <hermuliaanjelita@gmail.com>
2025-06-08 20:26:15 +0700

    Assessment 3

---
+++ app/src/main/java/com/anjelitahp0044/assessment3_mobpro/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.anjelitahp0044.assessment3_mobpro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.anjelitahp0044.assessment3_mobpro.ui.theme.Assessment3_MobproTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Assessment3_MobproTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Assessment3_MobproTheme {
+        Greeting("Android")
+    }
+}
