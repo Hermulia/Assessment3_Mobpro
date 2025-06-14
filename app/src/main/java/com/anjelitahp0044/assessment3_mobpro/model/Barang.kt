4458c37dfc0b091b52c004ad717ed733ee2e5e68
Anjelita <hermuliaanjelita@gmail.com>
2025-06-14 19:45:01 +0700

    membuat model

---
+++ app/src/main/java/com/anjelitahp0044/assessment3_mobpro/model/Barang.kt
@@ -0,0 +1,10 @@
+package com.anjelitahp0044.assessment3_mobpro.model
+
+data class Barang(
+    val id: String,
+    val userId: String?,
+    val namaBarang: String,
+    val jumlah: Int,
+    val gambar: String?,
+    val deskripsi: String?
+)
