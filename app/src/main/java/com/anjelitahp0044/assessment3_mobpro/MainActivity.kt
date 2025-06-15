97181d1b362202fd3dd9d3cb9174bff612296e7f
Anjelita <hermuliaanjelita@gmail.com>
2025-06-16 01:22:29 +0700

    MainActivity

--- app/src/main/java/com/anjelitahp0044/assessment3_mobpro/MainActivity.kt
+++ app/src/main/java/com/anjelitahp0044/assessment3_mobpro/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.anjelitahp0044.assessment3_mobpro.screen.MainScreen
 import com.anjelitahp0044.assessment3_mobpro.ui.theme.Assessment3_MobproTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +13,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Assessment3_MobproTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Assessment3_MobproTheme {
-        Greeting("Android")
     }
 }
