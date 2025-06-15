2593bdb654025b0347050f200f89d07fdc2cbb38
Anjelita <hermuliaanjelita@gmail.com>
2025-06-16 01:21:20 +0700

    Barang

--- app/src/main/java/com/anjelitahp0044/assessment3_mobpro/model/Barang.kt
+++ app/src/main/java/com/anjelitahp0044/assessment3_mobpro/model/Barang.kt
@@ -2,8 +2,7 @@ package com.anjelitahp0044.assessment3_mobpro.model
 
 data class Barang(
     val id: String,
-    val namaBarang: String,
-    val gambar: String,
-    val jumlah: Int,
-    val deskripsi: String
+    val nama: String,
+    val deskripsi: String,
+    val imageId: String,
 )
